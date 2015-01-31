8e04264630c8d20708f4c8292c627e9da05913f9
nfantone <nfantone@gmail.com>
2015-01-31 12:53:01 -0300

    Debug RESET_KEY added (press spacebar to reset level)

--- core/src/ar/uba/fi/game/input/NinjaRabbitInputProcessor.java
+++ core/src/ar/uba/fi/game/input/NinjaRabbitInputProcessor.java
@@ -17,6 +17,7 @@ public class NinjaRabbitInputProcessor extends InputAdapter {
 	private final static int LEFT_KEY = Keys.A;
 	private final static int DUCK_KEY = Keys.S;
 	private final static int RIGHT_KEY = Keys.D;
+	private static final int RESET_KEY = Keys.BACKSPACE;
 
 	private final Entity character;
 
@@ -41,6 +42,9 @@ public class NinjaRabbitInputProcessor extends InputAdapter {
 			break;
 		case DUCK_KEY:
 			// character.execute(NinjaRabbit.DUCK);
+			break;
+		case RESET_KEY:
+			character.execute(NinjaRabbit.RESET);
 			break;
 		default:
 			break;
