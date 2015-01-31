063af51b3d4b3459c0800c4eb3fdaab69681b662
nfantone <nfantone@gmail.com>
2015-01-31 13:01:31 -0300

    LevelFactory created levels using default path to .tmx files (convention over configuration)

--- core/src/ar/uba/fi/game/map/LevelFactory.java
+++ core/src/ar/uba/fi/game/map/LevelFactory.java
@@ -3,7 +3,6 @@
  */
 package ar.uba.fi.game.map;
 
-import net.dermetfan.gdx.physics.box2d.Box2DMapObjectParser;
 import ar.uba.fi.game.physics.BodyEditorLoader;
 
 import com.badlogic.gdx.assets.AssetManager;
@@ -18,6 +17,7 @@ import com.badlogic.gdx.physics.box2d.World;
  *
  */
 public final class LevelFactory {
+	private static final String LEVEL_MAP_FILE = "map/level.%s.tmx";
 	private static final String COLLECTIBLES_LAYER = "collectibles";
 
 	private LevelFactory() {
@@ -25,10 +25,8 @@ public final class LevelFactory {
 	};
 
 	/**
-	 * Loads a tiled map described by the given <code>.tmx</code> file. Loads objects as Box2D
-	 * bodies and fixtures as a {@link Box2DMapObjectParser} would. Creates
-	 * {@link CollectibleRenderer} for collectible layers (every layer which name starts with
-	 * "collectible").
+	 * Loads a tiled map described by the given level number. Creates {@link CollectibleRenderer}
+	 * for collectible layers (every layer which name starts with "collectible").
 	 *
 	 * @param world
 	 *            The Box2D {@link World} used to create bodies and fixtures from objects layers in
@@ -39,19 +37,17 @@ public final class LevelFactory {
 	 * @param assets
 	 *            {@link AssetManager} from where to get audio and graphics resources for the
 	 *            collectibles.
-	 * @param filename
-	 *            Path to a <code>.tmx</code> file.
+	 * @param level
+	 *            Number of the level the returned render should draw.
 	 * @param unitScale
 	 *            Pixels per unit.
 	 *
 	 * @return A new {@link LevelRenderer}, ready to render the map, its bodies and collectibles.
 	 */
 	public static LevelRenderer create(final World world, final BodyEditorLoader loader, final Batch batch,
-			final AssetManager assets, final String filename,
+			final AssetManager assets, final byte level,
 			final float unitScale) {
-		TiledMap tiledMap = new TmxMapLoader().load(filename);
-		Box2DMapObjectParser objectParser = new Box2DMapObjectParser(unitScale);
-		objectParser.load(world, tiledMap);
+		TiledMap tiledMap = new TmxMapLoader().load(String.format(LEVEL_MAP_FILE, level));
 		LevelRenderer renderer = new LevelRenderer(tiledMap, assets, batch, unitScale);
 
 		for (MapLayer ml : tiledMap.getLayers()) {
