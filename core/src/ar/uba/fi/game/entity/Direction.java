3871d3b8b70b89bdc5b8990ee64e133d07290298
nfantone <nfantone@gmail.com>
2015-01-31 12:49:05 -0300

    Added javadoc

--- core/src/ar/uba/fi/game/entity/Direction.java
+++ core/src/ar/uba/fi/game/entity/Direction.java
@@ -9,8 +9,18 @@ package ar.uba.fi.game.entity;
  *
  */
 public enum Direction {
-	LEFT("left"), RIGHT("right");
+	/**
+	 * The {@link Entity} is facing left.
+	 */
+	LEFT("left"),
+	/**
+	 * The {@link Entity} is facing right.
+	 */
+	RIGHT("right");
 
+	/**
+	 * A human readable description of the value of the enum. Useful for printing and debugging.
+	 */
 	private String direction;
 
 	private Direction(final String direction) {
